5168198a20ad04fc2627b7ccf7ae7e29e896f729
rakeshchouhan <rakesh.chouhan5j85@gmail.com>
2018-12-13 06:53:58 +0530

    Added the recording and download functionalities

--- src/js/main.js
+++ src/js/main.js
@@ -6,9 +6,31 @@ const constraints = {
     audio: true, // mandatory.
     video: true
 };
+var recorderChunk = [];
+function recordMedia(stream){
+    var video =   document.getElementById("video");
+    video.srcObject = stream;
+    var recorder = null;
+    try{
+        recorder = new MediaRecorder(stream, {mimetype:"video/webm"});
+    }catch(e){
+        console.log('fail to record media');
+        return;
+    }
+
+    recorder.ondataavailable=(event) =>{
+       // console.log("Recording the video of chunk size :: "+event.data.size;
+        recorderChunk.push(event.data);
+    }
+
+    
+    recorder.start(100);
+
+}
+
 function init(){
     var video =   document.getElementById("video");
-    var media = navigator.getUserMedia(constraints, successCallBack, errorCallBack);
+    navigator.mediaDevices.getUserMedia(constraints, successCallBack, errorCallBack).then(recordMedia);
     video.setAttribute("height","300px");
     video.setAttribute("weight","300px");
     document.getElementById("play").addEventListener("click",function(){
@@ -21,6 +43,7 @@ function init(){
     document.getElementById("pause").addEventListener("click",function(){
         if(video.isConnected && !video.paused){
             video.pause();
+            console.log(recorderChunk);
         }
         
     },false)
@@ -34,6 +57,17 @@ function init(){
      function errorCallBack(){
          alert("Unable to stream data");
      }
+
+     function downloadEventHandler(event){
+         var blob = new Blob(recorderChunk,{type:'video/webm'});
+         var url = window.URL.createObjectURL(blob);
+         this.href=url;
+         this.target="_blank";
+         this.download = "recorded.webm";
+
+     }
+
+     document.getElementById("download").addEventListener('click',downloadEventHandler, false);
      
     
 }
